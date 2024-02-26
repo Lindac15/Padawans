44888c9184a212cf8fb82917c7350bbb8f533147
Lindac15 <158802214+Lindac15@users.noreply.github.com>
2024-02-25 20:59:41 -0800

    first

---
+++ Day1/python/pw_demo.py
@@ -0,0 +1,24 @@
+# print("welcome to CT Rhett!")
+# name = 'Rhett'
+# print ("Welcome to CT" + name)
+
+# print(f"welcome to CT {name})
+
+
+def namer(f_name):
+    print(f"WELCOME WELCOME WELCOME CT {f_name}")
+    print("this will run too!")
+    
+# namer("Jenequa")
+# namer("Zack")
+# namer("Rhett")
+# namer ("Harrison")
+print(namer ("Adrian") + namer("Bryce"))
+print(namer("Adrian")==3)
+# print(namer("Adrian")== 3)
+# name2 = "Adrian"
+
+
+# def adder (num1, num2):
+#   return num1 + num2
+# print(adder(5, 6))
